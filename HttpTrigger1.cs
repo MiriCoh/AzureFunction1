4262a48e3710548b2faa19cfc0e13cfe015aebce
user1 <user1@DESKTOP-49VMUKQ>
2022-07-17 17:01:29 +0300

    branch-zure-fanction

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -17,7 +17,7 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request i want to merge this project !!!.");
+            log.LogInformation("C# HTTP trigger function processed a request i want to merge this.");
 
             string name = req.Query["name"];
 
@@ -26,7 +26,7 @@ namespace Company.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "Hi This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
